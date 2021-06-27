d31ca62e61d45c91cbd8e80a84eeaabcdfeb667e
Ziva <birulangit1996@gmail.com>
2021-06-27 16:32:26 +0700

    handle not found meal

--- src/components/home/ForYou.js
+++ src/components/home/ForYou.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import MealResult from '../MealResult';
+
 const ForYou = () => {
   const [meals, setMeals] = useState([]);
 
@@ -12,27 +13,9 @@ const ForYou = () => {
 
   return (
     <div className="grid lg:grid-cols-4 grid-cols-3">
-      {meals.map((recipe) => (
-        <div key={recipe.idMeal}>
-          <Link to={`recipe/${recipe.idMeal}`}>
-            <div className="rounded shadow-lg m-1">
-              <img
-                className="rounded-t-lg"
-                src={recipe.strMealThumb}
-                alt="Meal"
-                loading="lazy"
-              />
-              <div className="recipe-information px-2 py-4">
-                <div className="text-sm truncate">{recipe.strMeal}</div>
-                <p className="text-gray-700 truncate overflow-clip overflow-hidden h-2/4 text-base">
-                  {recipe.strArea}
-                </p>
-                <p className="text-gray-700 truncate overflow-clip overflow-hidden h-2/4 text-base">
-                  {recipe.strCategory}
-                </p>
-              </div>
-            </div>
-          </Link>
+      {meals.map((meal) => (
+        <div key={meal.idMeal}>
+          <MealResult meal={meal} />
         </div>
       ))}
     </div>
--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -13,43 +13,56 @@ const Search = () => {
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
       .then((response) => response.json())
-      .then((data) => setRecipe(data.meals[0]))
+      .then((data) => setRecipe(data.meals))
       .catch((error) => console.log(error));
   }, [search]);
 
   return (
-    <div className="p-4 text-center">
-      <form>
-        <input
-          type="search"
-          className="bg-purple-100 px-3 py-2 shadow rounded border-0"
-          value={search}
-          placeholder="search for meal"
-          onChange={handleChangeSearch}
-        />
-      </form>
+    <div className="p-4">
+      <div className="text-center">
+        <form>
+          <input
+            type="search"
+            className="bg-purple-100 px-3 py-2 shadow rounded border-0"
+            value={search}
+            placeholder="search for meal"
+            onChange={handleChangeSearch}
+          />
+        </form>
+      </div>
+
       <div className="search-container">
         <div className="mt-4">
           {search ? (
-            <Link to={`recipe/${recipe.idMeal}`}>
-              <div className="w-card h-card rounded shadow-lg">
-                <img
-                  className="rounded-t-lg"
-                  src={recipe.strMealThumb}
-                  alt="Meal"
-                  loading="lazy"
-                />
-                <div className="recipe-information px-2 py-4">
-                  <div className="text-sm truncate">{recipe.strMeal}</div>
-                  <p className="text-gray-700 truncate overflow-clip overflow-hidden h-2/4 text-base">
-                    {recipe.strArea}
-                  </p>
-                  <p className="text-gray-700 truncate overflow-clip overflow-hidden h-2/4 text-base">
-                    {recipe.strCategory}
-                  </p>
+            <div className="grid grid-cols-4">
+              {recipe ? (
+                recipe.map((meal) => (
+                  <Link to={`recipe/${meal.idMeal}`}>
+                    <div className="rounded shadow-lg m-1">
+                      <img
+                        className="rounded-t-lg"
+                        src={meal.strMealThumb}
+                        alt="Meal"
+                        loading="lazy"
+                      />
+                      <div className="meal-information px-2 py-4">
+                        <div className="text-sm truncate">{meal.strMeal}</div>
+                        <p className="text-gray-700 truncate overflow-clip overflow-hidden h-2/4 text-base">
+                          {meal.strArea}
+                        </p>
+                        <p className="text-gray-700 truncate overflow-clip overflow-hidden h-2/4 text-base">
+                          {meal.strCategory}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                ))
+              ) : (
+                <div className="py-20 text-center">
+                  <h1 className="text-center">No meal found</h1>
                 </div>
-              </div>
-            </Link>
+              )}
+            </div>
           ) : (
             <ForYou />
           )}
